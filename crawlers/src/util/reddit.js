8fd4d1ceb27df2d885cc238f1ac9132123afa6e3
ramos <rdiego26@gmail.com>
2017-07-02 20:15:57 -0300

    ADD method to validate params types .

--- crawlers/src/util/reddit.js
+++ crawlers/src/util/reddit.js
@@ -18,19 +18,26 @@ const redditUtil = {
 	createObject: function(points, title, url) {
 		var result = null;
 
-		try {
+		if(redditUtil.validateParams(points, title, url)) {
 			result = {
 				points: parseInt(points),
 				title: title,
 				url: redditUtil.checkAndValidateUrl(url)
 			};
-
-		} catch (ex) {
+		} else {
 			logger.warn("Invalid params when create object", { points: points, title: title, url: url });
 		}
 
 		return result;
 
+	},
+
+	validateParams: function(points, title, url) {
+		const isValidPoint = typeof points === "number";
+		const isValidTitle = typeof title === "string";
+		const isValidUrl = typeof url === "string";
+
+		return isValidPoint && isValidTitle && isValidUrl;
 	}
 
 };
